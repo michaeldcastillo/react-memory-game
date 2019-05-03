dcff5efedf96b4c90e727a8b7c2fe81ac07075d2
Mike Castillo Git <michaeldcastillo@hotmail.com>
2019-05-03 17:24:17 -0500

    Updates

---
+++ precache-manifest.83e61ca73ba288c667a8daae7f6b76bd.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fcaeb5a6de4d5e298a828632269ba41b",
+    "url": "/react-memory-game/index.html"
+  },
+  {
+    "revision": "e6d0ee7357445bdf6b0f",
+    "url": "/react-memory-game/static/css/main.2c5b044f.chunk.css"
+  },
+  {
+    "revision": "878eeff346544492bdd5",
+    "url": "/react-memory-game/static/js/2.6a8fbdc7.chunk.js"
+  },
+  {
+    "revision": "e6d0ee7357445bdf6b0f",
+    "url": "/react-memory-game/static/js/main.a4c1d781.chunk.js"
+  },
+  {
+    "revision": "e8b71f681a914c951703",
+    "url": "/react-memory-game/static/js/runtime~main.83a7a993.js"
+  },
+  {
+    "revision": "d044192d8116d7fbf33a832b8b85f794",
+    "url": "/react-memory-game/static/media/bs-200x200-01.d044192d.jpg"
+  },
+  {
+    "revision": "6de216d4262e59636898c42f84c16b10",
+    "url": "/react-memory-game/static/media/bs-200x200-02.6de216d4.jpg"
+  },
+  {
+    "revision": "a0992850143147fdc93366cf0cb50e92",
+    "url": "/react-memory-game/static/media/bs-200x200-03.a0992850.jpg"
+  },
+  {
+    "revision": "ec0e079edae365d3503b22516016ea4e",
+    "url": "/react-memory-game/static/media/bs-200x200-04.ec0e079e.jpg"
+  },
+  {
+    "revision": "ae51ddedddc6e5c766c063042ecd1041",
+    "url": "/react-memory-game/static/media/bs-200x200-05.ae51dded.jpg"
+  },
+  {
+    "revision": "421d5cacfb81c2ba5fea0e8012716925",
+    "url": "/react-memory-game/static/media/bs-200x200-06.421d5cac.jpg"
+  },
+  {
+    "revision": "df28e3eb519a0fde1566a10903bbffc2",
+    "url": "/react-memory-game/static/media/bs-200x200-07.df28e3eb.jpg"
+  },
+  {
+    "revision": "a4b5c5698ee56c03fe20cb16ddb47963",
+    "url": "/react-memory-game/static/media/bs-200x200-08.a4b5c569.jpg"
+  },
+  {
+    "revision": "9b2216e4245a411d8c04a8d63fe3a94b",
+    "url": "/react-memory-game/static/media/bs-200x200-09.9b2216e4.jpg"
+  },
+  {
+    "revision": "c14b7f2eb4559f19572f8bcd2ca7c1e6",
+    "url": "/react-memory-game/static/media/bs-200x200-10.c14b7f2e.jpg"
+  },
+  {
+    "revision": "c54cc4dfd0720df2d5527d8b1e108606",
+    "url": "/react-memory-game/static/media/bs-200x200-11.c54cc4df.jpg"
+  },
+  {
+    "revision": "6532d599484a38f4175ab2eb747da804",
+    "url": "/react-memory-game/static/media/bs-200x200-12.6532d599.jpg"
+  }
+]);
